a263b38cc9f7d54fcf3570d081dfa200ef6e9283
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-12-17 20:10:27 -0500

    Updates

---
+++ precache-manifest.aae7e15e7c22488db91ba585241cff22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6efa0b555f57834c06acd2ca613867",
+    "url": "/PassportReactExpressNode/index.html"
+  },
+  {
+    "revision": "e04fbfebbaaa26d084cf",
+    "url": "/PassportReactExpressNode/static/css/main.9f41e47c.chunk.css"
+  },
+  {
+    "revision": "5e5e8d81b90e4d792fab",
+    "url": "/PassportReactExpressNode/static/js/2.2fec15be.chunk.js"
+  },
+  {
+    "revision": "5206620010af2b11c5bf9aa2294a44a0",
+    "url": "/PassportReactExpressNode/static/js/2.2fec15be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e04fbfebbaaa26d084cf",
+    "url": "/PassportReactExpressNode/static/js/main.c7652043.chunk.js"
+  },
+  {
+    "revision": "534b30c16c822e0dec52",
+    "url": "/PassportReactExpressNode/static/js/runtime-main.9250c85e.js"
+  },
+  {
+    "revision": "f6fb6f5d6d05fadbc757f2d356a6ee71",
+    "url": "/PassportReactExpressNode/static/media/footer-github.f6fb6f5d.png"
+  },
+  {
+    "revision": "d566a5b6c9a8d070e72bc176595f132c",
+    "url": "/PassportReactExpressNode/static/media/instagram.d566a5b6.png"
+  },
+  {
+    "revision": "5044823b521b725f7c6ea8d32e336028",
+    "url": "/PassportReactExpressNode/static/media/spotify.5044823b.png"
+  }
+]);
